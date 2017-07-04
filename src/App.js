8a5413ed19d98189a960ccf9394361ffb72a5087
Daniel Rector <rector.danielk@gmail.com>
2017-07-04 15:10:59 -0400

    stripped down to barebones. added links in a footer to social media. focus on spinning logo, then scroll down to look at projects w links.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,3 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
-
-
 import React, { Component } from 'react';
 import './App.css';
 import logo8 from './website-backgrounds/logo8.JPG';
@@ -28,12 +6,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <div className="Menu-button">
-            <h1>Menu</h1>
-          </div>
-        </div>
         <img src={logo8} className="App-logo" alt="logo" />
+
+        <div className="App-body">
+          <h1>Projects I've worked on.</h1>
+        </div>
+
+        <div className="App-footer">
+          <ul>
+            <li ><a target="_blank" href="https://soundcloud.com/drector1">Music</a></li>
+            <li ><a target="_blank" href="https://twitter.com/danielkrector">Twitter</a></li>
+            <li ><a target="_blank" href="https://www.instagram.com/drector1/">Instagram</a></li>
+            <li ><a href="mailto:rector.danielk@gmail.com?subject=Hello&body=Hi">Email</a></li>
+          </ul>
+        </div>
       </div>
     );
   }
